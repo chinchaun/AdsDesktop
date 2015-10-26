644e8a4c01f3ea4411a1003b25a18a47e2ca88f3
Jean Jacques Delannoy <jjdelannoy@hotmail.com>
2015-10-26 09:06:02 -0700

    Added Reports

---
+++ AdsDesktop/frmReportes.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace AdsDesktop
+{
+    partial class frmReportes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            this.PacienteBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.EstudioBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            ((System.ComponentModel.ISupportInitialize)(this.PacienteBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.EstudioBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PacienteBindingSource
+            // 
+            this.PacienteBindingSource.DataSource = typeof(Negocio.Paciente);
+            // 
+            // EstudioBindingSource
+            // 
+            this.EstudioBindingSource.DataSource = typeof(Negocio.Estudio);
+            // 
+            // reportViewer1
+            // 
+            this.reportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            reportDataSource1.Name = "Pacientes";
+            reportDataSource1.Value = this.PacienteBindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "AdsDesktop.rptPacientes.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(0, 0);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.Size = new System.Drawing.Size(1027, 758);
+            this.reportViewer1.TabIndex = 0;
+            // 
+            // frmReportes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1027, 758);
+            this.Controls.Add(this.reportViewer1);
+            this.Name = "frmReportes";
+            this.Text = "frmReportes";
+            this.Load += new System.EventHandler(this.frmReportes_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PacienteBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.EstudioBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.BindingSource PacienteBindingSource;
+        private System.Windows.Forms.BindingSource EstudioBindingSource;
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+    }
+}
---
+++ AdsDesktop/frmReportes.cs
@@ -0,0 +1,26 @@
+﻿using Negocio;
+using System;
+using System.Windows.Forms;
+
+namespace AdsDesktop
+{
+    public partial class frmReportes : Form
+    {
+        public frmReportes()
+        {
+            InitializeComponent();
+        }
+
+        private void frmReportes_Load(object sender, EventArgs e)
+        {
+            var rptDataSource = new Microsoft.Reporting.WinForms.ReportDataSource("Pacientes", Paciente.GetAll());
+            this.reportViewer1.LocalReport.DataSources.Add(rptDataSource);
+            this.reportViewer1.RefreshReport();
+        }
+
+        private void reportViewer1_Load(object sender, EventArgs e)
+        {
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}
